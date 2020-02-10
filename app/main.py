d1dc986333b0c057c598ac08fc579c137a898370
Cindy <cindyunrau@uvic.ca>
2020-02-09 18:12:20 -0800

    This is a worm

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import json
 import os
 import random
 import bottle
+import queue
 
 from api import ping_response, start_response, move_response, end_response
 
@@ -9,6 +10,7 @@ from api import ping_response, start_response, move_response, end_response
 @bottle.route('/')
 def index():
     return '''
+    This is worm yes!
     Battlesnake documentation can be found at
        <a href="https://docs.battlesnake.com">https://docs.battlesnake.com</a>.
     '''
@@ -45,23 +47,108 @@ def start():
     """
     print(json.dumps(data))
 
-    color = "#00FF00"
-
+    #data['headType'] = 'bwc-earmuffs'
+    #data['tailType'] = 'bwc-ice-skate'
+    #data['color'] = '#add8e6'
+    color = "#add8e6"
     return start_response(color)
+
 
 
 @bottle.post('/move')
 def move():
     data = bottle.request.json
-
-    """
-    TODO: Using the data from the endpoint request object, your
-            snake AI must choose a direction to move in.
-    """
     print(json.dumps(data))
 
     directions = ['up', 'down', 'left', 'right']
+    value = data['turn'] % 12
+    health = data['you']['health']
+    food = data['board']['food'][0]
+    height = data['board']['height']
+    width = data['board']['width']
+    head = data['you']['body'][0]
+    avoid = []
+    num_snakes = len(data['board']['snakes'])
+
+    # appends the coordinates of the snakes to an array of coordinates to avoid
+    for i in range(num_snakes):
+        for j in range(len(data['board']['snakes'][i]['body'])):
+            string = '(' + str(data['board']['snakes'][i]['body'][j]['x']) + "," + str(data['board']['snakes'][i]['body'][j]['y']) + ")"
+            avoid.append(string)
+        
+
+    # appends the coordinates of the borders to the array of coordinates to avoid
+    for i in range(height):
+        string = "(" + '-1' + ',' + str(i) + ")"
+        avoid.append(string)
+        string = "(" + str(width) + ',' + str(i) + ")"
+        avoid.append(string)
+    for i in range(width):
+        string = "(" + str(i) + ',' + '-1' + ")"
+        avoid.append(string)
+        string = "(" + str(i) + ',' + str(height) + ")"
+        avoid.append(string)
+    
     direction = random.choice(directions)
+    # Finds food based on the first element in "food"
+    # Goes left/right until the x matches, then goes up/down
+    if health < 50:
+        if food['x'] < head['x']:
+            direction = 'left'
+            print("err: 1")
+        elif food['x'] > head['x']:
+            direction = 'right'
+            print("err: 2")
+        elif food['x'] == head['x']:
+            if food['y'] < head['y']:
+                direction = "up"
+                print("err: 3")
+            elif food['y'] > head['y']:
+                direction = 'down'
+                print("err: 4")
+    elif value == 0 or value == 1 or value == 2:
+        direction = 'up'
+        print("err: 6")
+    elif value == 4 or value == 5 or value == 3:
+        direction = 'right'
+        print("err: 7")
+    elif value == 7 or value == 8 or value == 6:
+        direction = 'down'
+        print("err: 8")
+    elif value == 10 or value == 11 or value == 9:
+        direction = 'left' 
+        print("err: 9")
+
+
+    if direction == 'left':
+        coord = "(" + str(head['x']-1) + ',' + str(head['y']) + ")"
+    elif direction == 'right':
+        coord = "(" + str(head['x']+1) + ',' + str(head['y']) + ")"
+    elif direction == 'up':
+        coord = "(" + str(head['x']) + ',' + str(head['y']-1) + ")"
+    else:
+        coord = "(" + str(head['x']) + ',' + str(head['y']+1) + ")"
+
+    if coord in avoid:
+        print("err: 10")
+        direction = 'up'
+        coord = "(" + str(head['x']) + ',' + str(head['y']-1) + ")"
+        if coord in avoid:
+            print("err: 11")
+            direction = 'down'
+            coord = "(" + str(head['x']) + ',' + str(head['y']+1) + ")"
+            if coord in avoid:
+                print("err: 12")
+                direction = 'right'
+                coord = "(" + str(head['x']+1) + ',' + str(head['y']) + ")"
+                if coord in avoid:
+                    print("err: 13")
+                    direction = 'left'
+                    
+
+    print(avoid)
+    print(coord)
+    print(direction)
 
     return move_response(direction)
 
